12f6281cf8b94e2b35c7ac0ff8bd49fe45026a17
Frank <frankmckechnie@gmail.com>
2020-01-04 22:52:30 +0000

    Fixed test

--- Tests/utils.test.js
+++ Tests/utils.test.js
@@ -2,6 +2,6 @@ import utils from '../src/js/utils';
 
 test('test get a random Int From Range', () => {
     const value = utils.randomIntFromRange(1,5);
-    expect(value).toBeGreaterThanOrEqual(6);
+    expect(value).toBeGreaterThanOrEqual(1);
     expect(value).toBeLessThanOrEqual(5);
 });
