71f5122801ecf98dbf498fc9a6fdabdb816bbb0d
Frank <frankmckechnie@gmail.com>
2020-01-26 21:41:35 +0000

    Added game options

---
+++ src/js/menu.js
@@ -0,0 +1,30 @@
+/**
+ * 
+ * Game menu
+ * 
+ */
+
+class Menu {
+
+    // menu
+
+    constructor(selector, hooks){
+        this.menu = document.querySelector(selector);
+
+        // gameMode
+        // gameLevel
+
+    }
+
+    getOptions(){
+        let options = this.menu.querySelectorAll('.js-options');
+    }
+
+
+
+}
+
+
+const menu = new Menu('.js-menu');
+
+
